3a84deb48917e549ecd66b58129523a1ee3f86e2
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-11-09 17:26:37 +0100

    stampa a schermo ma non copia nel file json

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const http = require("http");
 const dotenv = require("dotenv");
+const fs = require("fs");
+//const { log } = require("console");
 
 dotenv.config();
 
@@ -7,18 +9,36 @@ const port = process.env.PORT || 8080;
 const host = process.env.HOST || "localhost";
 
 http
-    .createServer((req, res)=>{
+    .createServer((req, res)=>{    
         res.writeHead(200, {"Content-Type": "text/html"});
-        res.end('prova')
+        res.end(frase) 
     })
     .listen(port,host, ()=>{
         const serverUrl = `http://${host}:${port}`
         console.log(serverUrl);
+        fetchData()
     })
         
+    let frase;  
+
     const fetchData = () => {
         fetch(`https://api.chucknorris.io/jokes/random`)
         .then(response => response.json())
-        .then(data => console.log(data));
-        };
-        fetchData()
+        .then(data => {frase = data.value
+            console.log(frase)       
+        });
+        }; 
+        
+
+        // fs.writeFileSync('norrisDb.json', frase, function(err){
+        //      if(err){
+        //          if(err) throw err;
+        //          console.log('saved')
+        //      }
+        //  })
+
+    
+
+        
+
+        
